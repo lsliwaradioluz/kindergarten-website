333b70a4e48fc4385fd1b8f2162389617e5ea838
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-03-17 16:14:16 +0100

    change title meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { 'http-equiv': 'Content-Language', content: 'pl-PL' },
       { 
         hid: 'description', 
         name: 'description', 
